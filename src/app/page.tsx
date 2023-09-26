c78fdbe5f0f3c3269d94af85be4e088f5cd70a89
minh160496 <leminh16041996@gmail.com>
2023-09-26 16:01:33 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,86 @@
 import { Home } from "@/features/home";
 
 const getPost = async () => {
-  const api_url = process.env.API_URL || "https://eaof.vn/wp-json/wp/v2";
-  const res = await fetch(
-    `${api_url}/posts?_embed&per_page=3&status=publish&page=1`,
-    {
-      next: { revalidate: 3600 },
-    }
-  );
-  const errorCode = res.ok ? false : res.status;
+  const api_url = process.env.API_URL || "";
 
-  const totalPosts = res.headers.get("X-WP-Total");
-  const posts = await res?.json();
+  try {
+    //get all categories
+    const resCats = await fetch(`${api_url}/categories`, {
+      next: { revalidate: 1800 },
+    });
+    const cats: any[] = (await resCats.json()) || [];
+    const newCat = cats?.find((cat) => cat.name === "Tin Tức");
+    const idNew = newCat?.id;
+    const notifiCat = cats?.find((cat) => cat.name === "Thông báo");
+    const idNotifi = notifiCat?.id;
 
-  const postsWithFeaturedImages: any[] = posts?.map((post: any) => {
-    const featured_image =
-      post._embedded?.["wp:featuredmedia"]?.[0]?.source_url || null;
+    //get posts category==='tin-tuc'
+    const resNews = await fetch(
+      `${api_url}/posts?_embed&per_page=3&status=publish&page=${1}&categories=${idNew}`,
+      {
+        next: { revalidate: 1800 },
+      }
+    );
+    const totalNews = resNews.headers.get("X-WP-Total");
+    const news: any[] = (await resNews?.json()) || [];
+    const newsWithFeaturedImages: any[] =
+      news?.length > 0
+        ? news?.map((post: any) => {
+            const featured_image =
+              post._embedded?.["wp:featuredmedia"]?.[0]?.source_url || null;
+
+            return {
+              ...post,
+              featured_image,
+            };
+          })
+        : [];
+
+    //get posts category==='thong-bao'
+    const resNotifis = await fetch(
+      `${api_url}/posts?_embed&per_page=3&status=publish&page=${1}&categories=${idNotifi}`,
+      {
+        next: { revalidate: 1800 },
+      }
+    );
+    const totalNotifis = resNotifis.headers.get("X-WP-Total");
+    const notifis: any[] = (await resNotifis?.json()) || [];
+    const motifisWithFeaturedImages: any[] =
+      notifis?.length > 0
+        ? notifis?.map((notifi: any) => {
+            const featured_image =
+              notifi._embedded?.["wp:featuredmedia"]?.[0]?.source_url || null;
+
+            return {
+              ...notifi,
+              featured_image,
+            };
+          })
+        : [];
 
     return {
-      ...post,
-      featured_image,
+      news: newsWithFeaturedImages || [],
+      notifis: motifisWithFeaturedImages || [],
+      totalNews: totalNews || "0",
+      totalNotifis: totalNotifis || "0",
     };
-  });
-
-  return {
-    errorCode,
-    posts: postsWithFeaturedImages,
-    totalPosts,
-  };
+  } catch (error) {
+    console.log(error);
+    return {
+      news: [],
+      notifis: [],
+      totalNews: "0",
+      totalNotifis: "0",
+    };
+  }
 };
 
 const HomePage = async () => {
-  const { posts } = await getPost();
+  const { news, notifis } = await getPost();
 
   return (
     <main>
-      <Home posts={posts || []} />
+      <Home news={news || []} notifis={notifis || []} />
     </main>
   );
 };
